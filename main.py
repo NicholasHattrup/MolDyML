5fe5c867ddd0f95836025a68a869ec11323f558e
NicholasHattrup <nhattrup@andrew.cmu.edu>
2023-03-03 15:43:02 -0500

    ML model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import ase 
 from src.train.initial_configuation import Initial
 from src.train import md
+from src.random import random
 from src.dynamics.calculator import CombinedSystem
 # General script where I will build a python package around the workflow I hope to devise below
 xyz_file = '/Users/nickhattrup/Documents/research/MolDyML/geom/3_monomer/3_monomer_conf0_siman.xyz'
@@ -20,11 +21,12 @@ print(configs.configs)
 #md.combine_pos_force_xyz(pos_file='/Users/nickhattrup/Documents/research/MolDyML/geometries/training_data/dynamics/MD/3_monomer/positions.xyz',
 #                         force_file='/Users/nickhattrup/Documents/research/MolDyML/geometries/training_data/dynamics/MD/3_monomer/forces.xyz')
 
-#md.get_unique_frames('/Users/nickhattrup/Documents/research/MolDyML/geometries/training_data/dynamics/MD/3_monomer/positions.xyz')
 
+'''
+_, unique_indexes=md.get_unique_frames('/home/nhattrup/MolDyML/data/3_monomer/positions.xyz')
 
-
-
+random.extract_configurations('data/3_monomer/combined.xyz', unique_indexes)
+'''
 
 
 if __name__ == '__main__':
